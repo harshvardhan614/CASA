69870184da43b02cfdc12276bf559a116ca7df66
Harshvardhan Rathore <rharshvardhan614@gmail.com>
2023-01-17 06:43:00 +0500

    solve responsiveness issue and bugs

--- Home/home_script.js
+++ Home/home_script.js
@@ -19,39 +19,85 @@ $(".menu-bar").click(function () {
   navbar.classList.remove('active');      
 });
 
-// for home-main slider 
-const slides = document.querySelectorAll(".img-slide");
-var counter = 0;
+// FAQ part 
+const faqs = document.querySelectorAll('.faq');
 
-slides.forEach(
-  (slide, index) => {
-    slide.style.left = `${index * 100}%`
-  }
-)
+faqs.forEach((faq) =>{
+    faq.addEventListener('click', ()=> {
+        faq.classList.toggle('active');
+        console.log("faq clicked");
+    })
+});
 
-const goPrev = () =>{
-    counter--;
-    console.log("me clicked")
-    slideImage()
-}
-
-const goNext = () =>{
-    counter++;
-    console.log("clicked")
-    slideImage()
-}
-
-const slideImage = () => {
-    slides.forEach(
-        (slide) => {
-            console.log("clicked me")
-            if(counter >4  ){
-                counter === 0;
-                slide.style.transform = `translateX(-0% )`
-            }
-            slide.style.transform = `translateX(-${counter * 100}% )`
+// kitchen image owl-carousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        650:{
+            items:2
+        },
+        1000:{
+            items:3
         }
-    )
-}
+    }
+})
 
+// swiper section for main image carousel 
+const swiper = new Swiper('.swiper-1', {
+    // Optional parameters  
+    autoplay:{
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    loop: true,  
+  
+     
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+  });
+  
+  const swiper2 = new Swiper(".swiper-container-2", {
+          // Optional parameters
+          //   direction: 'vertical', remove it and add new slideperview
+          autoplay:{
+          delay: 2000,
+          disableOnInteraction: false,
+          },loop: true,
+          slidesPerGroup: 1,
+          speed: 2000,
+          breakpoints: {
+            470: {
+              slidesPerView: 1,
+              // slidesPerGroup:0,
+              spaceBetween: 10,
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            820: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            1100: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+          },        
+  
+          // And if we need scrollbar
+          scrollbar: {
+            el: ".swiper-scrollbar",
+          },
+        });
 
